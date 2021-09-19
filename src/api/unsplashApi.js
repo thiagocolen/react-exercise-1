bc000289f1ada1825f0445e441883e13417654c9
Thiago Colen <thiago.souzacolen@gmail.com>
2021-09-18 23:53:36 -0300

    adding environment variable

--- src/api/unsplashApi.js
+++ src/api/unsplashApi.js
@@ -1,13 +1,16 @@
 import { mainActions } from "../store/mainSlice";
-
+require("dotenv").config();
 
 export const UnsplashApi = () => {
-
   return async (dispatch) => {
+    const unsplashUrlDomain = "https://api.unsplash.com/";
     const backgroundImageData = async () => {
-      const response = await fetch(`https://api.unsplash.com/photos/random/?topics=6sMVjTLSkeQ&client_id=????`, {
-        method: "GET",
-      });
+      const response = await fetch(
+        `${unsplashUrlDomain}photos/random/?topics=6sMVjTLSkeQ&client_id=${process.env.REACT_APP_UNSPLASH_ACCESS_KEY}`,
+        {
+          method: "GET",
+        }
+      );
       const data = await response.json();
       return data;
     };
